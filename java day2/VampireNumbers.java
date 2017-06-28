7b1680668edea1bbf8b7c3f42eef2f4ab8741892
Srinivas Kotla <srinivas.kotla@zemosolabs.com>
2017-06-28 15:24:14 +0530

    VampireNumbers.java code is modified

--- java day2/VampireNumbers.java
+++ java day2/VampireNumbers.java
@@ -29,7 +29,7 @@ public class VampireNumbers{
 		}
 		
 		k=0;
-		dummy = i*j
+		dummy = i*j;
 
 // separating the digits of the product of the fang pairs and placing them in another array
 		while(dummy !=0){
@@ -50,19 +50,24 @@ public class VampireNumbers{
 			else
 				count++;
 		}
-		
+//if all the cells of the arrays get matched and the number has even number of digits we set the isVamp flag to true.
 		if(count == arrayNum.length && k%2!=1)
 			isVamp = true;
 			
 	return isVamp;
 }
 	public static void main(String [] args){
-		int[] array = new int[100];
+		int[] array = new int[200];
 		int index = 0;
 
+		for(int i=0;i<array.length;i++){
+			array[i] = 1000000;
+		}
+
+//loop for finding the vampire numbers with 2 digit fangs.
 		for(int i = 10; i<100;i++){
 			for(int j = i; j<100; j++){
-				if(index == 100)
+				if(index == 200) 
 					break;
 
 				if(checkVampire(i,j) == true){
@@ -73,9 +78,10 @@ public class VampireNumbers{
 			}
 		}
 
+//loop for finding the vampire numbers with 2 digit fangs.
 		for(int i = 100; i<1000;i++){
 			for(int j = i; j<1000; j++){
-				if(index == 100)
+				if(index == 200)
 					break;
 
 				if(checkVampire(i,j) == true){
@@ -85,6 +91,7 @@ public class VampireNumbers{
 			}
 		}
 
+//sorting the array to get the first 100 vampire numbers
 		Arrays.sort(array);
 		for(int i=0;i<100;i++){
 			System.out.println(array[i]);
